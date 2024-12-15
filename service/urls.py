062b81bcec78aeacb8638730f3ed9a037abb8249
robinnayak <robinnayak86@gmail.com>
2024-12-15 15:53:40 +0545

    domain purchase before payment

--- service/urls.py
+++ service/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('domain-agreement/', views.domain_agreement, name='domain-agreement' ), # Added the service app url
     path('success/', views.success, name='success' ), # Added the service app url
     path('cancel/', views.cancel, name='cancel' ), # Added the service app url
-    path('checkout-session/', views.checkout_session, name='checkout-session' ), # Added the service app url
     path('stripe-webhook/',views.stripe_webhook, name='stripe-webhook'),
 ]
